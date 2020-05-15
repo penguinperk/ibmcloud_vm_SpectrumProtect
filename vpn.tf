1b5383e4f124ebf814d1a37a8e5896be4aa7b0b0
Scott <perk@penguinnorth.com>
2020-05-15 11:04:26 -0500

    resolve is deployment issue on peer address

--- vpn.tf
+++ vpn.tf
@@ -11,17 +11,11 @@ resource "ibm_is_vpn_gateway" "vpngateway" {
 }
 
 resource "ibm_is_vpn_gateway_connection" "VPNGatewayConnection" {
-  name          = "test2"
-  vpn_gateway   = ibm_is_vpn_gateway.vpngateway.id
-  peer_address  = ibm_is_vpn_gateway.vpngateway.public_ip_address
-  preshared_key = "123456"
-  local_cidrs = [ibm_is_subnet.subnet1.ipv4_cidr_block]
-  peer_cidrs = ["192.168.11.0/24"]
-  #peer_cidrs = [ibm_is_subnet.testacc_subnet1.ipv4_cidr_block]
-}
-
-
-
-output "vpnpublicip" {
-  value  = "ibm_is_vpn_gateway.vpngatway.public_ip_address"
+  name           = "test2"
+  vpn_gateway    = ibm_is_vpn_gateway.vpngateway.id
+  peer_address   = "98.222.12.23"
+  preshared_key  = "123456"
+  local_cidrs    = [ibm_is_subnet.subnet1.ipv4_cidr_block]
+  peer_cidrs     = ["192.168.11.0/24"]
+  admin_state_up = "true"
 }
